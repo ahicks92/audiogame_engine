5a20b4f307e2580c0db156e5176595d40d8ab87d
camlorn <camlorn38@gmail.com>
2014-03-01 14:53:00 -0500

    Finished py2exe/cx_freeze support.

--- game_engine/__init__.py
+++ game_engine/__init__.py
@@ -1,21 +1,21 @@
 import atexit
 
 # If this is imported, it needs to init SDL, and sdl must only ever be initialized once.
+import resource_finder
 import os
 import os.path
 import glob
-dll_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'dlls')
+dll_path = resource_finder.find_application_resource("", "dlls")
 os.environ["PYSDL2_DLL_PATH"] = dll_path
 import sdl2
 sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 atexit.register(sdl2.SDL_Quit)
 
-#for packing with py2exe:
 def find_datafiles():
 	"""Returns all the datafiles that must be packaged alongisde this module, along with the appropriate location for each."""
 	dll_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'dlls')
 	dll_files = glob.glob(os.path.join(dll_path, '*'))
-	return ('dlls', dll_files)
+	return [('dlls', dll_files)]
 
 from .event_responder import *
 from .main_loop import *
