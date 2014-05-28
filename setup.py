95bad5d574a689899c8c64134e7cc64803462afb
camlorn <camlorn38@gmail.com>
2014-05-28 14:56:47 -0400

    Rename to audiogame_engine.
    Game_engine is misleading and likely to name clash somewhere.
    
    --HG--
    rename : game_engine/__init__.py => audiogame_engine/__init__.py
    rename : game_engine/event_responder.py => audiogame_engine/event_responder.py
    rename : game_engine/keyboard.py => audiogame_engine/keyboard.py
    rename : game_engine/main_loop.py => audiogame_engine/main_loop.py
    rename : game_engine/mouse.py => audiogame_engine/mouse.py
    rename : game_engine/scheduler.py => audiogame_engine/scheduler.py
    rename : game_engine/screen.py => audiogame_engine/screen.py
    rename : game_engine/screen_stack.py => audiogame_engine/screen_stack.py

--- setup.py
+++ setup.py
@@ -4,13 +4,21 @@ from glob import glob
 __version__ = 0.1
 
 setup(
- name = 'game_engine',
- version = __version__,
- description = """A basic game event loop and window""",
- package_dir = {'game_engine': 'game_engine'},
- packages = find_packages(),
+	name = 'audiogame_engine',
+	version = __version__,
+	description = """A basic game event loop and window""",
+	author = "Austin Hicks and Christopher Toth",
+	license = "BSD",
+	package_dir = {'audiogame_engine': 'audiogame_engine'},
+	packages = find_packages(),
 	zip_safe = False,
 	package_data = {
-'game_engine' : ['dlls/*.dll']
-}
+		'audiogame_engine' : ['dlls/*.dll']
+	},
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Topic :: Games/Entertainment',
+	]
 )
